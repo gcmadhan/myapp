f48b109069b7701a52115ab37476622b06423af5
DiyaM <DiyaM@LAPTOP-A6GHAEKU>
2022-08-25 19:27:31 +0530

    updated

--- myapp.py
+++ myapp.py
@@ -1,5 +1,22 @@
 import streamlit as st
+from PyDictionary import PyDictionary
+from 
 
-st.text_input("Enter your Name")
-st.text_input("Enter Passord")
-st.button("Submit")
+dic = PyDictionary()
+st.balloons()
+
+
+w = st.text_input("Enter the word here") 
+m = dic.meaning(w)
+for i in m:
+    st.subheader(i)
+    for j in m.get(i):
+        st.write(j)
+
+st.subheader("Synonums")
+s=dic.synonym(w)
+st.write(s)
+
+st.subheader("Antonyms")
+a = dic.antonym(w)
+st.write(a)
